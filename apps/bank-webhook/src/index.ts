74e9d0d091b9e2b7669247a0d52a96da7ba35fad
devRojha <devraj227804@gamil.com>
2024-06-20 14:32:02 +0530

    file

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,50 @@
+
+import express from "express"
+import db from "@repo/db/client"
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+    //TODO: Add zod validation here?
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+})
+
+app.listen(3003, ()=>{
+    console.log("webHook Server online at 3000")
+})
